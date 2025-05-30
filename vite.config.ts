9aa078f9b9251fe82881a3a2564c2ee2584f7e6b
Nareshkumarbalamurugan <153281056+Nareshkumarbalamurugan@users.noreply.github.com>
2025-05-30 15:04:09 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import path from 'path'
 
 export default defineConfig({
-  base: '/metric-master-pro/', // Required for GitHub Pages
+  base: '/metric-master-pro/',
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
